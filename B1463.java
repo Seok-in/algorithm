8844922063b6da138dafdfb3f91f63776ef67c56
Hong Seok In <hrpt8@naver.com>
2022-10-01 12:34:56 +0900

    [ADD] B1463

---
+++ B1463.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class B1463 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N+1];
+        arr[0]=0;
+        arr[1]=0;
+        for(int i=2;i<N+1;i++) {
+            arr[i]=arr[i-1]+1;
+            if(i%2==0) {
+                arr[i] = Math.min(arr[i], arr[i/2]+1);
+            }
+            if(i%3==0) {
+                arr[i] = Math.min(arr[i], arr[i/3]+1);
+            }
+        }
+        System.out.println(arr[N]);
+    }
+}
